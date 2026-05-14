1501678a30732eb35eb1c10b38bdd6289cdb41ba
planetceles <mandecmande@gmail.com>
2026-05-14 22:06:18 +0200

    exercise 5 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        // using System.Collections.Generic;
+
+        List<int> numbers = new List<int>();
+        // int response = 1;
+        int numberList = -1;
+
+        // Console.Write("Enter a list of numbers (0 to quit): ");
+
+        
+
+        while (numberList != 0)
+        {
+            Console.Write("Enter a numbers (0 to quit): ");
+            string numList = Console.ReadLine();
+            numberList = int.Parse(numList);
+
+            if (numberList != 0)
+            {
+                numbers.Add(numberList);
+            }
+            // sum
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            Console.WriteLine($"The sum is: {sum}");
+
+            // average
+            float average = (float)sum / numbers.Count;
+
+            Console.WriteLine($"The average is: {average}");
+
+            // maximum
+            int max = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+
+
+            // Console.WriteLine($"List is: {numbers.Count}");
+            
+            
+            Console.WriteLine($"The largest is: {max}");
+
+            // Console.Write("You want add more? ");
+            // string answer = Console.ReadLine();
+            // response = int.Parse(answer);
+        }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,78 @@ using System;
 
 class Program
 {
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static int AddNumber(int first, int second)
+    {
+        // first = 5;
+        // second = 4;
+        int sum = first + second;
+        Console.WriteLine(sum);
+
+        return sum;
+    }
+    static string PromptUserName()
+    {
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        string fullName = firstName + " " + lastName;
+
+        return fullName;
+        // return firstName + lastName;
+        
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        string answer = Console.ReadLine();
+        int favNum = int.Parse(answer);
+
+        return favNum;
+    }
+    static int SquareNumber(int num)
+    {
+        int numSquared = num * num;
+
+        return numSquared;
+    }
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, your favorite number squared is: {squaredNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+        AddNumber(5, 8);
+
+        int result = AddNumber(3, 2);
+        Console.WriteLine(result);
+
+        // PromptUserName();
+        string name = PromptUserName();
+        Console.WriteLine($"Hello {name}");
+
+        int favNum = PromptUserNumber();
+        Console.WriteLine($"{name} your favorite number is: {favNum}");
+
+        int numSquared = SquareNumber(favNum);
+        Console.WriteLine($"{name}, your favorite number squared is: {numSquared}");
+
+        DisplayResult(name, numSquared);
+
+        
+
+        // void DisplaMessage()
+        // {
+        //     console.WriteLine("Welcome to the Program");
+        // }
+
     }
 }
